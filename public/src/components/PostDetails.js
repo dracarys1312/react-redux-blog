68caf8bef71d9136fb9d7b6773b4596aef79a000
Pt <dracarys1312@gmail.com>
2016-10-12 17:45:42 +0700

    delete div.container in postdetail

--- public/src/components/PostDetails.js
+++ public/src/components/PostDetails.js
@@ -28,11 +28,9 @@ class PostDetails extends Component {
     }
 
     return (
-      <div className="container">
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
-      </div>
     );
   }
 }
