0c771a5bbc22ce4346a7f62bd09f5110774dceaf
Pt <dracarys1312@gmail.com>
2016-10-12 16:14:46 +0700

    fix class -> className

--- public/src/components/header.js
+++ public/src/components/header.js
@@ -1,114 +1,140 @@
-import React, { Component, PropTypes } from 'react';
-import { Link } from 'react-router';
-
+import React, {Component, PropTypes} from 'react';
+import {Link} from 'react-router';
 
 class Header extends Component {
-  static contextTypes = {
-    router: PropTypes.object
-  };
+    static contextTypes = {
+        router: PropTypes.object
+    };
 
-  componentWillUnmount() {
-    //Important! If your component is navigating based on some global state(from say componentWillReceiveProps)
-    //always reset that global state back to null when you REMOUNT
-     this.props.resetMe();
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if(nextProps.deletedPost.error && nextProps.deletedPost.error.message) {//delete failure
-      alert(nextProps.deletedPost.error.message || 'Could not delete. Please try again.');
-    } else if(nextProps.deletedPost.post && !nextProps.deletedPost.error) {//delete success
-      this.context.router.push('/');
-    } else if(this.props.user.user && !nextProps.user.user) {//logout (had user(this.props.user.user) but no loger the case (!nextProps.user.user))
-      this.context.router.push('/');
-    }
-  }
-
-  renderSignInLinks(authenticatedUser) {
-    if(authenticatedUser) {
-      return (
-        <ul className="nav  nav-pills navbar-right">
-            <li style={{paddingRight: '10px'}} role="presentation">
-              <Link role="presentation" style={{color:'#996633',  fontSize: '17px'}} to="/profile">
-              {authenticatedUser.name}
-              </Link>
-            </li>
-            <li style={{paddingRight: '10px'}} role="presentation">
-              <a style={{color:'#996633',  fontSize: '17px'}}  onClick={this.props.logout} href="javascript:void(0)">
-              Log out
-              </a>
-            </li>
-        </ul>
-      );
+    componentWillUnmount() {
+        //Important! If your component is navigating based on some global state(from say componentWillReceiveProps)
+        //always reset that global state back to null when you REMOUNT
+        this.props.resetMe();
     }
 
-    return (
-      <ul className="nav  nav-pills navbar-right">
-          <li style={{paddingRight: '10px'}} role="presentation">
-            <Link  role="presentation" style={{color:'#996633',  fontSize: '17px'}} to="/signup">
-            Sign up
-            </Link>
-          </li>
-          <li style={{paddingRight: '10px'}} role="presentation">
-            <Link style={{color:'#996633',  fontSize: '17px'}} to="/signin">
-            Sign in
-            </Link>
-          </li>
-      </ul>
-   );
-  }
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.deletedPost.error && nextProps.deletedPost.error.message) { //delete failure
+            alert(nextProps.deletedPost.error.message || 'Could not delete. Please try again.');
+        } else if (nextProps.deletedPost.post && !nextProps.deletedPost.error) { //delete success
+            this.context.router.push('/');
+        } else if (this.props.user.user && !nextProps.user.user) { //logout (had user(this.props.user.user) but no loger the case (!nextProps.user.user))
+            this.context.router.push('/');
+        }
+    }
 
-	renderLinks() {
-		const { type, authenticatedUser } = this.props;
-		if(type === 'posts_index') {
-       return (
-        <div className="container">
-          <ul className="nav  nav-pills navbar-right">
-      			<li style={{paddingRight: '10px'}} role="presentation">
-      				<Link style={{color:'#337ab7',  fontSize: '17px'}} to="/posts/new">
-      				New Post
-    					</Link>
-            </li>
-    			</ul>
-         {this.renderSignInLinks(authenticatedUser)}
+    renderSignInLinks(authenticatedUser) {
+        if (authenticatedUser) {
+            return (
+                <ul className="nav  nav-pills navbar-right">
+                    <li style={{
+                        paddingRight: '10px'
+                    }} role="presentation">
+                        <Link role="presentation" to="/profile">
+                            {authenticatedUser.name}
+                        </Link>
+                    </li>
+                    <li style={{
+                        paddingRight: '10px'
+                    }} role="presentation">
+                        <a onClick={this.props.logout} href="javascript:void(0)">
+                            Log out
+                        </a>
+                    </li>
+                </ul>
+            );
+        }
 
-        </div>
-  		 );
-  	} else if(type === 'posts_new') {
-       return (
-        <div className="container">
-          {this.renderSignInLinks(authenticatedUser)}
-          <ul className="nav  nav-pills navbar-left">
-      			<li style={{paddingRight: '10px'}} role="presentation">
-      				<Link className="text-xs-right"  style={{color:'#337ab7',  fontSize: '17px'}}  to="/">Back To Index</Link>
-      			</li>
-    			</ul>
-        </div>
-  		 );
-  	} else if(type === 'posts_show') {
-  			return (
-  			 <div className="container">
-    			<ul className="nav  nav-pills navbar-left">
-      			<li style={{paddingRight: '10px'}} style={{color:'#337ab7',  fontSize: '17px'}}  role="presentation"><Link to="/">Back To Index</Link></li>
-    			</ul>
+        return (
+            <ul className="nav  nav-pills navbar-right">
+                <li style={{
+                    paddingRight: '10px'
+                }} role="presentation">
+                    <Link role="presentation" to="/signup">
+                        Sign up
+                    </Link>
+                </li>
+                <li style={{
+                    paddingRight: '10px'
+                }} role="presentation">
+                    <Link to="/signin">
+                        Sign in
+                    </Link>
+                </li>
+            </ul>
+        );
+    }
 
-    			<div className="navbar-form navbar-right" style={{paddingRight: '50px'}}>
-      			<button className="btn btn-warning pull-xs-right"  onClick={()=> {this.props.onDeleteClick()}}>Delete Post</button>
-      		</div>
-           {this.renderSignInLinks(authenticatedUser)}
-    	   </div>
-  		);
-  	}
-	};
+    renderLinks() {
+        const {type, authenticatedUser} = this.props;
+        if (type === 'posts_index') {
+            return (
+                <div className="container">
+                    <ul className="nav  nav-pills navbar-right">
+                        <li style={{
+                            paddingRight: '10px'
+                        }} role="presentation">
+                            <Link to="/posts/new">
+                                New Post
+                            </Link>
+                        </li>
+                    </ul>
+                    {this.renderSignInLinks(authenticatedUser)}
 
-	render() {
-			return (
-			 <nav className="navbar navbar-default navbar-static-top">
-			      <div id="navbar" className="navbar-collapse collapse">
-			      {this.renderLinks()}
-	      		</div>
-			 </nav>
-			);
-	}
+                </div>
+            );
+        } else if (type === 'posts_new') {
+            return (
+                <div className="container">
+                    {this.renderSignInLinks(authenticatedUser)}
+                    <ul className="nav nav-pills navbar-left">
+                        <li style={{
+                            paddingRight: '10px'
+                        }} role="presentation">
+                            <Link className="text-xs-right" to="/">Back To Index</Link>
+                        </li>
+                    </ul>
+                </div>
+            );
+        } else if (type === 'posts_show') {
+            return (
+                <div className="container">
+                    <ul className="nav  nav-pills navbar-left">
+                        <li style={{
+                            paddingRight: '10px'
+                        }} role="presentation">
+                            <Link to="/">Back To Index</Link>
+                        </li>
+                    </ul>
+
+                    <div className="navbar-form navbar-right" style={{
+                        paddingRight: '50px'
+                    }}>
+                        <button className="btn btn-warning pull-xs-right" onClick={() => {
+                            this.props.onDeleteClick()
+                        }}>Delete Post</button>
+                    </div>
+                    {this.renderSignInLinks(authenticatedUser)}
+                </div>
+            );
+        }
+    };
+
+    render() {
+        return (
+            <nav className="navbar nav-wrapper navbar-static-top">
+                <a href="#!" className="brand-logo">Happy Today</a>
+                <a href="#" data-activates="mobile-demo" className="button-collapse">
+                    <i className="material-icons">menu</i>
+                </a>
+                <ul className="hide-on-med-and-down">
+                    {this.renderLinks()}
+                </ul>
+                <ul className="side-nav" id="mobile-demo">
+                    {this.renderLinks()}
+                </ul>
+            </nav>
+        );
+    }
 }
 
 export default Header
