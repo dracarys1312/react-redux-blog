8e40020a29890380e77a46c242b71ea2f646c581
Pt <dracarys1312@gmail.com>
2016-10-12 17:55:50 +0700

    fix bug :bug:

--- public/src/components/PostDetails.js
+++ public/src/components/PostDetails.js
@@ -28,9 +28,11 @@ class PostDetails extends Component {
     }
 
     return (
+      <div className="">
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
+      </div>
     );
   }
 }
