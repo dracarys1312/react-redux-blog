f0d97214f2146e799f9866acddaf4cc29e37531e
Pt <dracarys1312@gmail.com>
2016-10-12 22:47:06 +0700

    change layout of ProfileCard

--- public/src/components/ProfileCard.js
+++ public/src/components/ProfileCard.js
@@ -6,12 +6,23 @@ export default class ProfileCard extends Component {
   render() {
     let user = this.props.user.user;
     return (
-      <div>
-        <div><h4>Name:</h4> {user && user.name}</div>
-        <div><h4>Username:</h4> {user && user.username}</div>
-        <div><h4>Email:</h4> {user && user.email}</div>
-        <br/><br/>        
-      </div>
+        <ul className="collection">
+            <li className="collection-item avatar">
+                <i className="material-icons prefix">person</i>
+                <h4>Name:</h4> {user && user.name}
+                <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+            </li>
+            <li className="collection-item avatar">
+                <i className="material-icons prefix">portrait</i>
+                <h4>Username:</h4> {user && user.username}
+                <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+            </li>
+            <li className="collection-item avatar">
+                <i className="material-icons prefix">email</i>
+                <h4>Email:</h4> {user && user.email}
+                <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+            </li>
+        </ul>
     );
   }
 }
